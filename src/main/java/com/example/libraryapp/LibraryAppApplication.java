1d9bffbda8963b5d97271f075a14967700734d60
ArtemkaGh0st1k <artem4uk.6class.october@yandex.ru>
2025-11-22 13:43:09 +0400

    Рефакторинг

--- src/main/java/com/example/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/example/libraryapp/LibraryAppApplication.java
@@ -1,14 +1,13 @@
-package com.example.libraryapp;  // Пакет вашего проекта (может быть другим, если вы изменили)
+package com.example.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  // Основная аннотация: включает авто-конфигурацию, сканирование компонентов и т.д.
+@SpringBootApplication
 public class LibraryAppApplication
-{  // Имя класса — обычно [Artifact]Application, где Artifact = library-app
-
+{
     public static void main(String[] args)
-    {  // Точка входа
-        SpringApplication.run(LibraryAppApplication.class, args);  // Запускает приложение
+    {
+        SpringApplication.run(LibraryAppApplication.class, args);
     }
 }
